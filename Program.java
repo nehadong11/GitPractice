102848c18a93182064f346ec56e3e7c8ccec0686
neha123 <nehadongare70666@gmail.com>
2022-08-17 23:55:09 +0530

    Add Successfully

---
+++ Program.java
@@ -0,0 +1,22 @@
+package git.sdm;
+
+import java.util.Stack;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		Stack st = new Stack();
+		
+		st.push(10);
+		st.push(20);
+		st.push(30);
+		st.push(40);
+		st.push(50);
+		
+		System.out.println(st);
+		
+	}
+
+}
